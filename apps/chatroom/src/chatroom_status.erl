3de17ffeda9395e9cd700adfe14fc6c1d3f1edf4
jiangyuhang17 <yuhangjiang17@foxmail.com>
2021-06-19 23:37:39 +0000

    [feature] websocket server example

--- apps/chatroom/src/chatroom_status.erl
+++ apps/chatroom/src/chatroom_status.erl
@@ -4,10 +4,10 @@
 
 -export([init/2]).
 
-init(Req, Opts) ->
-  IP = cowboy_util:get_ip(Req),
+init(Req0, State) ->
+  IP     = cowboy_util:get_ip(Req0),
   RetMsg = [{status, open}, {ip, list_to_binary(IP)}],
-  NReq = cowboy_req:reply(200, #{
+  Req    = cowboy_req:reply(200, #{
     <<"content-type">> => <<"application/json">>
-  }, json_util:encode(RetMsg), Req),
-  {ok, NReq, Opts}.
+  }, json_util:encode(RetMsg), Req0),
+  {ok, Req, State}.
--- apps/chatroom/src/connection.erl
+++ apps/chatroom/src/connection.erl
@@ -4,6 +4,12 @@
 
 -export([start_link/0]).
 
+-export([init/2]).
+
+-export([websocket_init/1, websocket_handle/2, websocket_info/2, terminate/3]).
+
+-record(state, {uname, ip}).
+
 start_link() ->
   {ok, WsPort} = config:get(chatroom, port),
   ?INFO("roomchat port: ~p------------------", [WsPort]),
@@ -13,7 +19,56 @@ start_link() ->
       {'_', ?MODULE, []}
     ]}
   ]),
-  {ok, Pid} = cowboy:start_clear(http, [{port, WsPort}], #{
+  cowboy:start_clear(http, [{port, WsPort}], #{
     env => #{dispatch => Dispatch}
-  }),
-  {ok, Pid}.
+  }).
+
+init(Req0, _State) ->
+  process_flag(trap_exit, true),
+  IP = cowboy_util:get_ip(Req0),
+  {cowboy_websocket, Req0, #state{ip = IP}}.
+
+websocket_init(State) ->
+  ?DEBUG("[~p] ~p websocket_init, ip=~p", [?MODULE, self(), State#state.ip]),
+  {ok, State}.
+
+websocket_handle(NetMsg = {login, {uname, UName}}, State) ->
+  do_log_msg_(up, NetMsg, State),
+  {reply, {ok, login}, State#state{uname = UName}};
+websocket_handle(NetMsg, State) ->
+  do_log_msg_(up, NetMsg, State),
+  {ok, State}.
+
+websocket_info(NetMsg, State) ->
+  do_log_msg_(info, NetMsg, State),
+  {ok, State}.
+
+terminate(Reason, _Req, State) ->
+  %% TODO chatroom delete UName from ets about broadcast
+  terminate_(Reason, State);
+terminate(Reason, _Req, State) ->
+  terminate_(Reason, State).
+
+terminate_(Reason, State = #state{uname = UName}) ->
+  case Reason of
+    stop ->
+      %% 服务器主动{reply, {close, Payload}}关闭
+      ?INFO("[~p]~p UName:~p, websocket closed by server, state:~p", [?MODULE, self(), UName, State]);
+    timeout ->
+      %% 客户端idle timeout
+      ?INFO("[~p]~p UName:~p, websocket closed due to timeout, state:~p", [?MODULE, self(), UName, State]);
+    {remote,Code,Payload} ->
+      %% 客户端主动关闭
+      ?INFO("[~p]~p UName:~p, websocket closed by client (code:~p,payload:~p), state:~p", [?MODULE, self(), UName, Code, Payload, State]);
+    Error ->
+      %% 其他错误
+      ?INFO("[~p]~p UName:~p, websocket closed unexpectedly (reason:~p), state:~p", [?MODULE, self(), UName, Error, State])
+  end,
+  ok.
+
+do_log_msg_(up, NetMsg, State) ->
+  ?DEBUG("[~p] ~p UP NetMsg=~p State=~p", [?MODULE, self(), NetMsg, State]);
+do_log_msg_(down, NetMsg, State) ->
+  ?DEBUG("[~p] ~p DOWN NetMsg=~p State=~p", [?MODULE, self(), NetMsg, State]);
+do_log_msg_(info, NetMsg, State) ->
+  ?DEBUG("[~p] INFO ~p NetMsg=~p State=~p", [?MODULE, self(), NetMsg, State]).
